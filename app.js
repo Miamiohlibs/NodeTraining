465cfa2d2a37d510cc3975cef66515901c85f491
h0w4rds <howardha@miamioh.edu>
2022-09-02 16:19:25 -0400

    Clean commit of Unit 1; strategy barebone units.

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
-// const { response } = require('express');
-const express = require('express'); // use the express module
-// const path = require('path');
-// const { allowedNodeEnvironmentFlags } = require('process');
-// instantiate an express instance called "app"
+const express = require('express');
 const app = express();
-// register view engine
-// app.set('views', './views');
-// app.set('view engine', 'ejs');
 
-// app.use(express.static('views'));
-app.use(express.urlencoded({ extended: true}));
-
-// https://www.youtube.com/watch?v=yXEesONd_54&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=7
 app.get('/', (req, res) => {
     res.send(`
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
     <h1>Project</h1>
         <div class="container">
             <h2>Unit 1</h2>
@@ -47,36 +38,3 @@ app.post('/',(req,res)=>{
 });
 
 app.listen(3000);
-
-// static folder = public
-// app.use(express.static(path.join(__dirname, 'public')));
-// var stations = JSON.parse(lite.json);
-// https://www.youtube.com/watch?v=1iysNUrI3lw
-// allow user to search for stations on
-// station.country
-// station["name"].en
-// to find correlating
-// station.id
-
-// https://dev.meteostat.net/api/stations/hourly.html#parameters
-// https://dev.meteostat.net/api/stations/nearby.html#example
-// GET https://meteostat.p.rapidapi.com/stations/nearby
-// https://dev.meteostat.net/bulk/#quick-start
-
-// const axios = require("axios");
-
-// const options = {
-//   method: 'GET',
-//   url: 'https://meteostat.p.rapidapi.com/stations/monthly',
-//   params: {station: '10637', start: document.getElementById('startD'), end: document.getElementById('endD')},
-//   headers: {
-//     'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-//     'X-RapidAPI-Host': 'meteostat.p.rapidapi.com'
-//   }
-// };
-
-// axios.request(options).then(function (response) {
-// 	console.log(response.data);
-// }).catch(function (error) {
-// 	console.error(error);
-// });
