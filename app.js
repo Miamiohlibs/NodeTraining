e4e7afdbabd08aa8fd4df99dcadd9c7e6a9fbe2a
h0w4rds <howardha@miamioh.edu>
2022-09-08 17:11:06 -0400

    Data from form through view does not work.

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
-app.use(express.urlencoded({ extended: true}));
-
 app.set('view engine', 'ejs');
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.render(`index`);
+    res.render('index',
+        { title: 'NodeTraining' } 
+    );
+});
+
+app.post('/output', (req, res) => {
+    const { location, sdate, edate } = req.body;
+    console.log(req.body);
+    res.render('index', {
+        title: 'NodeTraining',
+        location }
+    );
 });
 
 app.listen(3000);
